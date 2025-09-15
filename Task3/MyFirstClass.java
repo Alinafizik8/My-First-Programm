13e2c8acbcfe80ab2ac7a15d919e4c1495efe77d
Owlina <98256692+Alinafizik8@users.noreply.github.com>
2025-09-15 10:02:05 +0300

    Update MyFirstClass.java

--- Task3/MyFirstClass.java
+++ Task3/MyFirstClass.java
@@ -1,7 +1,42 @@
 class MyFirstClass {
- 	public void main(String[] s) {
-     	for (int i = 0; i < s.length; i++) {
-        System.out.println(s[i]);
-      }
- 	}
+    public static void main(String[] s) {
+        MySecondClass o = new MySecondClass(1,1);
+        System.out.println(o.minus());
+        for (int i = 0; i<=8; i++){
+            for (int j = 0; j<=8; j++){
+                o.setA(i);
+                o.setB(j);
+                System.out.println(o.minus());
+                System.out.println(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+class MySecondClass {
+    private int a;
+    private int b;
+    public MySecondClass(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public MySecondClass() {
+        this.a = 0;
+        this.b = 0;
+    }
+    public int get_a(){
+        return a;
+    }
+    public int get_b(){
+        return b;
+    }
+    public void setA(int newA) {
+        a = newA;
+    }
+    public void setB(int newB) {
+        b = newB;
+    }
+    public int minus(){
+        return a-b;
+    }
 }
